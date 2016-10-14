e3accbda5327fc2d5d33bdb10ff3eefcd427347f
sim-d <sim-dou@hotmail.fr>
2016-10-15 00:16:04 +0200

    resize grid // bloc animation orientation

--- app/src/main/java/app_yams/yamsscore/Main_Page.java
+++ app/src/main/java/app_yams/yamsscore/Main_Page.java
@@ -1,48 +1,60 @@
 package app_yams.yamsscore;
 
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.ZoomControls;
 
-import java.util.Hashtable;
-
+import static app_yams.yamsscore.R.id.grille;
 
 
 public class Main_Page extends AppCompatActivity {
 
-    public int flag = 0;
 
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main__page);
 
 
-        if (flag == 0) {
-            final FrameLayout zoom = (FrameLayout) findViewById(R.id.grille);
+
+        if(savedInstanceState == null){
+            // effet de zoom au lancement uniquement
+            final FrameLayout zoom = (FrameLayout) findViewById(grille);
             final Animation zoomAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom);
             zoom.startAnimation(zoomAnimation);
-            flag = 1;
         }
 
-        flag = 1;
+
+
+        // resize proportion et placement de la grille ( a chaque demmarage et orientation )
+         final FrameLayout grilleFrame = (FrameLayout) findViewById(grille);
+        grilleFrame.setX(25.0f);
+        grilleFrame.setY(25.0f);
+        grilleFrame.setScaleX(1.0f);
+        grilleFrame.setScaleY(1.0f);
+
 
     }
 
 
 
 
+    protected void onStart() {
+        super.onStart();
 
+    }
 
-
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+    /*Save your data to be restored here
+    Example : outState.putLong("time_state", time); , time is a long variable*/
+        super.onSaveInstanceState(outState);
+    }
 
 
 
