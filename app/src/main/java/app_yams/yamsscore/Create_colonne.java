0720f8119612ebb15772a0629f7b45c6f0d139e9
sim-d <sim-dou@hotmail.fr>
2016-10-16 17:25:23 +0200

    Scale et move image

---
+++ app/src/main/java/app_yams/yamsscore/Create_colonne.java
@@ -0,0 +1,24 @@
+package app_yams.yamsscore;
+
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
+
+/**
+ * Created by sim-d on 16/10/2016.
+ */
+
+public class Create_colonne {
+
+
+    public void creation(float PosX, float PosY, FrameLayout Layout) {
+
+
+        CheckBox checkBox1 = new CheckBox(Layout.getContext());
+
+        Layout.addView(checkBox1);
+
+
+
+    }
+
+}
