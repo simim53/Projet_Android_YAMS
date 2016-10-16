cd35079ac74bc49cca8500557414a07c438b6b05
sim-d <sim-dou@hotmail.fr>
2016-10-16 17:24:41 +0200

    Scale et move image

--- app/src/main/java/app_yams/yamsscore/Main_Page.java
+++ app/src/main/java/app_yams/yamsscore/Main_Page.java
@@ -11,14 +11,18 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 
+
 public class Main_Page extends AppCompatActivity {
 
-    private float scale = 1f;
-    private ScaleGestureDetector SGD;
-    private ImageView ImageGrille;
-    private FrameLayout Layout_Principal;
+    private float scale ;
+    public ScaleGestureDetector SGD;
+    public ImageView ImageGrille;
+    public FrameLayout Layout_Principal;
+    public RelativeLayout RelativeLayout_;
+    public Create_colonne colonne1;
     private float dX;
     private float dY;
+
 
     @Override
 
@@ -27,24 +31,41 @@ public class Main_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main__page);
-        ImageGrille = (ImageView) findViewById(R.id.imageView);
-        Layout_Principal = (FrameLayout) findViewById(R.id.Layout_Principal);
-        RelativeLayout RelativeLayout_ = (RelativeLayout)findViewById(R.id.RelativeLayout);
+
+
 
 
         // condition valide si 1er lancement
         if (savedInstanceState == null) {
+
             // effet de zoom au lancement uniquement
             final FrameLayout zoom = (FrameLayout) findViewById(R.id.Layout_Principal);
             final Animation zoomAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom);
             zoom.startAnimation(zoomAnimation);
+
+
+
         }
 
 
-
+        ImageGrille = (ImageView) findViewById(R.id.imageView);
+        Layout_Principal = (FrameLayout) findViewById(R.id.Layout_Principal);
+        RelativeLayout_ = (RelativeLayout)findViewById(R.id.RelativeLayout);
 
         //creation listener geste et scale
         SGD = new ScaleGestureDetector(this, new ScaleListener());
+
+
+
+
+        //colonne1 = new Create_colonne();
+        //colonne1.creation(10,10,Layout_Principal);
+
+
+
+
+
+
 
 
 
